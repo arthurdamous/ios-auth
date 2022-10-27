d204e03078acfde47a71087cb76cadc748ecd670
Arthur Damous <arthur.damous@gmail.com>
2022-10-27 15:38:25 -0300

    Making api call passing the params on the body of the request

---
+++ AuthApp/AuthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthAppApp.swift
+//  AuthApp
+//
+//  Created by Arthur Damous on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AuthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthApp/ContentView.swift
@@ -0,0 +1,101 @@
+//
+//  ContentView.swift
+//  AuthApp
+//
+//  Created by Arthur Damous on 27/10/22.
+//
+
+import SwiftUI
+
+struct Constants{
+    static let BASE_URL = "https://auth-api-heroku.herokuapp.com"
+}
+
+struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
+    var body: some View {
+        
+        NavigationView {
+            ZStack{
+                Color.blue.ignoresSafeArea()
+                Circle().scale(1.7).foregroundColor(.white.opacity(0.15))
+                Circle().scale(1.35).foregroundColor(.white)
+                
+                VStack{
+                    
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .border(.red, width: CGFloat(wrongUsername))
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login"){
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                }
+            }
+            .navigationBarHidden(true)
+        }
+        
+    }
+    
+    func authenticateUser(username: String, password: String){
+        guard let url = URL(string : "\(Constants.BASE_URL)/auth/login") else {
+            return
+        }
+        
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let body: [String: AnyHashable] = [
+            "email": username.lowercased(),
+            "password": password
+        ]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do{
+                let response = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                print("SUCCESS: \(response)")
+                
+            }catch{
+                print(error)
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
